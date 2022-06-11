85d4a8ebac64c1bc8624a5ebb10d709a4535e0d6
Marcel G <arksol89@gmail.com>
2022-06-11 23:11:02 +0200

    Added media queries in the header component
    Signed-off-by: Marcel G <arksol89@gmail.com>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,20 @@ import Style from './Header.css';
 const PageHeader = ({ }) => {
 
 
-
-
-
   return (
     <div id='header' className='header'>
-  
+      <div>
+        <p>ICON</p>
+        <p>TITLE</p>
+        <p>CONTACT</p>
+
+      </div>
+      <div>
+        <p>NEW_POST</p>
+        <p>LOGIN</p>
+      </div>
+
+
     </div>
   )
 }
