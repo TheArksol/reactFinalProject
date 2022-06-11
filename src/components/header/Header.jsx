a2c2ad71cdac13b2d1fe373c4220c71d257ded7e
Marcel G <arksol89@gmail.com>
2022-06-11 22:53:25 +0200

    Initial
    Signed-off-by: Marcel G <arksol89@gmail.com>

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Style from './Header.css';
+
+const PageHeader = ({ }) => {
+
+
+
+
+
+  return (
+    <div id='header' className='header'>
+  
+    </div>
+  )
+}
+
+export default PageHeader;
